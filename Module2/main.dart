51f85018a90d0f79949576c9e7959d0ecbf65e72
HetviGusani <hgusani05@gmail.com>
2026-02-23 22:37:19 +0530

    commit

---
+++ Module2/main.dart
@@ -0,0 +1,21 @@
+import 'package:assignments/27.dart';
+
+import '26.dart';
+import '28.dart';
+import '30.dart';
+import '31.dart';
+import '32.dart';
+
+Future<void> main() async {
+
+  // print("Starting number stream...\n");
+  //
+  // // Listen to the stream
+  // await for (int number in generateNumbers(5)) {
+  //   print("Received: $number");
+  // }
+  //
+  // print("\nStream completed after reaching the limit.");
+  //
+}
+

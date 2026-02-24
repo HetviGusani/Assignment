aac69fbf4689f44ace48422623f7efde175ad3d3
HetviGusani <hgusani05@gmail.com>
2026-02-24 22:58:36 +0530

    second

---
+++ Module3/2.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Counter extends StatefulWidget {
+  const Counter({super.key});
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Floating Action Button")),
+      body: Center(child: Text("Counter : $count")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            count++;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
